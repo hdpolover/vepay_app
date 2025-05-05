8edff8e96c3cd9fffb95f47f13a3c98766d75bfb
Rizal Abdul Basith <rizalabdulbasith@maxxitani.id>
2025-05-05 14:32:01 +0700

    fix add scroll view to login and remove snackbar on login via google

--- lib/screens/auth/intro.dart
+++ lib/screens/auth/intro.dart
@@ -25,100 +25,102 @@ class _IntroState extends State<Intro> {
           child: Padding(
             padding:
                 EdgeInsets.symmetric(horizontal: 2 * 0.1, vertical: h * 0.05),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FancyShimmerImage(
-                  boxFit: BoxFit.cover,
-                  height: h * 0.25,
-                  width: w * 0.5,
-                  imageUrl: AppInfoService().getValueByKey('web_splash_image')!,
-                  errorWidget: Image.network(
-                      'https://vectorified.com/images/user-icon-1.png'),
-                ),
-                SizedBox(
-                  height: h * 0.05,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: w * 0.07),
-                  child: Text(
-                    AppInfoService().removeHtmlTags(
-                        AppInfoService().getValueByKey('web_splash_title')!),
-                    softWrap: true,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FancyShimmerImage(
+                    boxFit: BoxFit.cover,
+                    height: h * 0.25,
+                    width: w * 0.5,
+                    imageUrl: AppInfoService().getValueByKey('web_splash_image')!,
+                    errorWidget: Image.network(
+                        'https://vectorified.com/images/user-icon-1.png'),
+                  ),
+                  SizedBox(
+                    height: h * 0.05,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: w * 0.07),
+                    child: Text(
+                      AppInfoService().removeHtmlTags(
+                          AppInfoService().getValueByKey('web_splash_title')!),
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: h * 0.03,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: w * 0.1),
+                    child: Text(
+                      AppInfoService().removeHtmlTags(
+                          AppInfoService().getValueByKey('web_splash_desc')!),
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium
+                          ?.copyWith(color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(
+                    height: h * 0.1,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: w * 0.07),
+                    child: SizedBox(
+                      height: 48,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: ColorManager.primary, // background
+                          foregroundColor: Colors.white, // foreground
                         ),
-                  ),
-                ),
-                SizedBox(
-                  height: h * 0.03,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: w * 0.1),
-                  child: Text(
-                    AppInfoService().removeHtmlTags(
-                        AppInfoService().getValueByKey('web_splash_desc')!),
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium
-                        ?.copyWith(color: Colors.grey),
-                  ),
-                ),
-                SizedBox(
-                  height: h * 0.1,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: w * 0.07),
-                  child: SizedBox(
-                    height: h * 0.06,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: ColorManager.primary, // background
-                        foregroundColor: Colors.white, // foreground
+                        child: const Text('Daftar'),
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Register(),
+                            ),
+                          );
+                        },
                       ),
-                      child: const Text('Daftar'),
-                      onPressed: () async {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Register(),
-                          ),
-                        );
-                      },
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: h * 0.02,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: w * 0.07),
-                  child: SizedBox(
-                    height: h * 0.06,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white54, // background
-                        foregroundColor: ColorManager.primary, // foreground
+                  SizedBox(
+                    height: h * 0.02,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: w * 0.07),
+                    child: SizedBox(
+                      height: 48,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white54, // background
+                          foregroundColor: ColorManager.primary, // foreground
+                        ),
+                        child: const Text('Masuk'),
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Login(),
+                            ),
+                          );
+                        },
                       ),
-                      child: const Text('Masuk'),
-                      onPressed: () async {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Login(),
-                          ),
-                        );
-                      },
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
